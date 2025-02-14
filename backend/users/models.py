fc1245b15c5ace1053032581bdf4af13003f3902
Nurmukhamed <nurik.berikbolov@gmail.com>
2025-02-14 23:49:32 +0100

    Added custom User model with phone and city fields, configured Django admin with BaseUserAdmin, and applied MySQL migrations

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,9 +1,16 @@
+# users/models.py
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# class User(AbstractUser):
-#     phone = models.CharField(max_length=15, unique=True)
-#     city = models.CharField(max_length=100, blank=True, null=True)
-#
-#     def __str__(self):
-#         return self.username
+class City(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class User(AbstractUser):
+    phone = models.CharField(max_length=15, unique=True, null=True, blank=True)
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, related_name='residents')
+
+    def __str__(self):
+        return self.username
